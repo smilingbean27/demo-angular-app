832f051878312fcec5e848ad1849d54f64fb37a2
Lokesh <lokeshisrani1996@gmail.com>
2020-12-16 18:02:27 +0530

    Some small changes in Weather service

---
+++ src/app/services/api_key.ts
@@ -0,0 +1,3 @@
+var api_key = '8f057ed690d7c138d0d65ff2b6c4cd94';
+
+export {api_key};
--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, empty, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
+import {api_key} from './api_key';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,7 @@ import { catchError } from 'rxjs/operators';
 export class WeatherService {
   
   private baseURL = 'http://api.openweathermap.org/data/2.5/weather?q=';
-  private suffixURL = '&appid=8f057ed690d7c138d0d65ff2b6c4cd94';
+  private suffixURL = '&appid='+ api_key;
 
   constructor(private http: HttpClient) {
 
